1b7a7a4f874f0187e36bcad28a01c3666cfae631
Eddie Harris <harrisejm@gmail.com>
2018-08-10 12:33:27 -0700

    add display all doctors feature

--- src/main.js
+++ src/main.js
@@ -15,7 +15,8 @@ $(document).ready(function()
       $('#paragraphs').val();
 
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors/004b64c28d73d5bb85432a500d3facd3?user_key=0d9b3d2e941d5fb5fc6b1ecdc6baf06d`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&limit=100000&user_key=0d9b3d2e941d5fb5fc6b1ecdc6baf06d`;
+    //  let url = `https://api.betterdoctor.com/2016-03-01/doctors/004b64c28d73d5bb85432a500d3facd3?user_key=0d9b3d2e941d5fb5fc6b1ecdc6baf06d`;
       request.onreadystatechange = function()
       {
         if(this.readyState === 4 && this.status === 200)
@@ -29,9 +30,22 @@ $(document).ready(function()
 
       let getElements = function(response)
       {
-         //let list = response.join().split(",").join(", ");
-        //let list = response;
-        $('#showIt').text(`${response.data.practices[0].name}`);
+      let arr = [];
+      let docDictionary = {};
+      for (let i = 0; i < response.data.length; i++) {
+      arr.push(response.data[i].profile.first_name + " " + response.data[i].profile.last_name + " " + response.data[i].profile.title);
+      let newDoctor = response.data[i].profile.first_name + " " + response.data[i].profile.last_name + " " + response.data[i].profile.title;
+
+      docDictionary[response.data[i].uid] = newDoctor;
+
+      }
+
+
+        // let list = response.join().split(",").join(", ");
+        //let list = response.data.practices[0].name;
+        //let list = arr;
+        let list = docDictionary["0b10e40e7e5bb627994059bad16fed4c"];
+        $('#showIt').text(`${list}`);
       }
     });
     });
